3f655ec95935a19ef91dfe4e766aa9a6ab764069
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2025-06-20 21:23:10 +0200

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css';
+import App from './App.tsx';
+import Challenges from './challenges/Challenges.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="coding-challenges/">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="challenge">
+          <Route path="star-rating" element={<Challenges />} />
+        </Route>
+        {/* Add more routes here if needed */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
